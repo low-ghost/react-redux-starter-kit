7de239ebe5b46442a21b5c7649dba2640403dfaf
Mike Bagwell <lowghost.clone@gmail.com>
2016-08-21 13:12:24 -0400

    added createReducer and tests

---
+++ src/utils/createReducer.js
@@ -0,0 +1,7 @@
+export default function createReducer (initialState, actionHandlers) {
+  return (state = initialState, action) => {
+    const handler = actionHandlers[action.type]
+
+    return handler ? handler(state, action) : state
+  }
+}
